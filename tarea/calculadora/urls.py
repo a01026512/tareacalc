101a9f5ff7845e57cbcb6c514b80ce58a1d4f44d
Esteban Zepeda <estzepeda@outlook.com>
2023-03-03 19:28:33 -0600

    Update 3/03/23

--- tarea/calculadora/urls.py
+++ tarea/calculadora/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('procesamiento', views.procesamiento, name='procesamiento'),
+    path('lista', views.lista, name='lista'),
+    path('suma', views.suma, name='suma'),
+    path('resta', views.resta, name='resta'),
+    path('multiplicacion', views.multiplicacion, name='multiplicacion'),
+    path('division', views.division, name='division'),
 ]
--- tarea/calculadora/views.py
+++ tarea/calculadora/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpResponse
+from json import loads,dumps
+from django.views.decorators.csrf import csrf_exempt
+from .models import Reto
+
+class Fraccion:
+    def __init__(self, num, den):
+        self.num = num
+        self.den = den
+    def toJSON(self):
+        return dumps(self, default=lambda o:o.__dict__, sort_keys=False, indent=4)
 
 # Create your views here.
 def nuvea():
@@ -12,3 +22,87 @@ def procesamiento(request):
     nombre = request.POST['nombre']
     nombre = nombre.title()
     return render(request, 'proceso.html', {'name':nombre})
+
+def lista(request):
+    jugadores = Reto.objects.all() #select * from Reto 
+    return render(request, 'datos.html',{'lista_jugadores':jugadores})
+
+@csrf_exempt
+def suma(request):
+    print("Ingrese 2 fracciones para suma")
+    body_unicode = request.body.decode('utf-8')
+    body=loads(body_unicode)
+    num1 = body['numerador1']
+    den1 = body['denominador1']
+
+    num2 = body['numerador2']
+    den2 = body['denominador2']
+
+    if(den1 == den2):
+        num_r =num1+num2
+        den_r =den1
+    else: 
+        num_r = (num1*den2)+(num2*den1)
+        den_r = den1*den2
+    
+    resultado = Fraccion(num_r,den_r)
+    resultado_json = resultado.toJSON()
+    return HttpResponse(resultado_json,content_type ="text/json-comment-filtered")
+
+@csrf_exempt
+def resta(request):
+    print("Ingrese 2 fracciones para resta")
+    body_unicode = request.body.decode('utf-8')
+    body=loads(body_unicode)
+    num1 = body['numerador1']
+    den1 = body['denominador1']
+
+    num2 = body['numerador2']
+    den2 = body['denominador2']
+
+    if(den1 == den2):
+        num_r =num1-num2
+        den_r =den1
+    else: 
+        num_r = (num1*den2)-(num2*den1)
+        den_r = den1*den2
+    
+    resultado = Fraccion(num_r,den_r)
+    resultado_json = resultado.toJSON()
+    return HttpResponse(resultado_json,content_type ="text/json-comment-filtered")
+
+@csrf_exempt
+def multiplicacion(request):
+    print("Ingrese 2 fracciones para multi")
+    body_unicode = request.body.decode('utf-8')
+    body=loads(body_unicode)
+    num1 = body['numerador1']
+    den1 = body['denominador1']
+
+    num2 = body['numerador2']
+    den2 = body['denominador2']
+
+    num_r =num1*num2
+    den_r = den1*den2
+    
+    resultado = Fraccion(num_r,den_r)
+    resultado_json = resultado.toJSON()
+    return HttpResponse(resultado_json,content_type ="text/json-comment-filtered")
+
+@csrf_exempt
+def division(request):
+    print("Ingrese 2 fracciones para div")
+    body_unicode = request.body.decode('utf-8')
+    body=loads(body_unicode)
+    num1 = body['numerador1']
+    den1 = body['denominador1']
+
+    num2 = body['numerador2']
+    den2 = body['denominador2']
+
+    num_r =num1*den2
+    den_r = num2*den1
+    
+    resultado = Fraccion(num_r,den_r)
+    resultado_json = resultado.toJSON()
+    return HttpResponse(resultado_json,content_type ="text/json-comment-filtered")
